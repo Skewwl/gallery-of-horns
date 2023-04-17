95d361a50622af81450d0c69e3c7ac816557caa0
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-17 12:22:26 -0700

    made three components but images not rendering

--- src/Main.js
+++ src/Main.js
@@ -5,11 +5,11 @@ import HornedBeast from "./HornedBeast";
 class Main extends React.Component{
     render(){
         return(
-            <Main>
+            <div>
                 <HornedBeast title={hbarray[0].title} imageURL={hbarray[0].imageurl} description={hbarray[0].description} />
                 <HornedBeast title={hbarray[1].title} imageURL={hbarray[1].imageurl} description={hbarray[1].description} />
                 <HornedBeast title={hbarray[2].title} imageURL={hbarray[2].imageurl} description={hbarray[2].description} />
-            </Main>
+            </div>
         )
     }
 };
