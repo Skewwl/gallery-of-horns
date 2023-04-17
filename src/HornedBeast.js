fb68ef3e0319255b4f57c39a54442050b1834a4b
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-17 12:05:50 -0700

    finished lab but images notrendering

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-class HornedBeast extends React.Component{
-    render(){
-        return(
-            <h2>{}</h2>
-            <img />
-            <p></p>
+class HornedBeast extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.imageURl} alt={this.props.title} title={this.props.title} />
+                <p>{this.props.description}</p>
+            </div>
         )
     }
 };
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
+import hbarray from "./hbarray";
+import HornedBeast from "./HornedBeast";
 
 class Main extends React.Component{
     render(){
         return(
-            <h1>Gallery Of Horns</h1>
+            <Main>
+                <HornedBeast title={hbarray[0].title} imageURL={hbarray[0].imageurl} description={hbarray[0].description} />
+                <HornedBeast title={hbarray[1].title} imageURL={hbarray[1].imageurl} description={hbarray[1].description} />
+                <HornedBeast title={hbarray[2].title} imageURL={hbarray[2].imageurl} description={hbarray[2].description} />
+            </Main>
         )
     }
 };
