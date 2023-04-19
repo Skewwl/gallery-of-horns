40e033fff455f3e27c408e33cb16650ebb7dde84
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-19 11:39:05 -0700

    stretch completed

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-
 class Header extends React.Component{
     render(){
         return(
-            <h1>Gallery Of Horns</h1>
+            <h1 onClick={this.props.onClick}>Gallery Of Horns</h1>
         )
     }
 };
