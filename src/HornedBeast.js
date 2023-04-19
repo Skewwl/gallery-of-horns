227b81e8bba9e3d9789a23fb4bf9c69bbf0f1c5c
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-19 08:11:56 -0700

    touchups from following along in class

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            helloCounter: 0
+        }
+    }
+
+    incrementHello = () => {
+        this.setState({
+            helloCounter: this.state.helloCounter + 1
+        })
+    }
+
     render() {
         return (
             <div>
                 <h2>{this.props.title}</h2>
                 <img src={this.props.imageURL} alt={this.props.title} title={this.props.title} />
                 <p>{this.props.description}</p>
+                <button onClick={this.incrementHello}>Add a like</button>
             </div>
         )
     }
