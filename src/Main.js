4b3b34e0a9e861509fbbadedd29811aa73e2ee65
rhettb253 <rhettbeardemphl@hotmail.com>
2023-04-20 21:14:57 -0700

    added full array and got all buttons to change color

--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,24 @@ import hbarray from "./hbarray";
 import HornedBeast from "./HornedBeast";
 import './HornedBeasts.css';
 
-class Main extends React.Component{
-    render(){
-        return(
+class Main extends React.Component {
+    render() {
+
+        return (
             <div className="hornedBeasts">
-                <HornedBeast title={hbarray[0].title} imageURL={hbarray[0].imageurl} description={hbarray[0].description} update={this.props.update} stateColor={this.props.currentColor} />
-                <HornedBeast title={hbarray[1].title} imageURL={hbarray[1].imageurl} description={hbarray[1].description} update={this.props.update} stateColor={this.props.currentColor} />
-                <HornedBeast title={hbarray[2].title} imageURL={hbarray[2].imageurl} description={hbarray[2].description} update={this.props.update} stateColor={this.props.currentColor} />
-            </div>
+                {/* <HornedBeast title={hbarray[0].title} image_url={hbarray[0].image_url} description={hbarray[0].description} update={this.props.update} stateColor={this.props.currentColor} />
+                <HornedBeast title={hbarray[1].title} image_url={hbarray[1].image_url} description={hbarray[1].description} update={this.props.update} stateColor={this.props.currentColor} />
+                <HornedBeast title={hbarray[2].title} image_url={hbarray[2].image_url} description={hbarray[2].description} update={this.props.update} stateColor={this.props.currentColor} /> */}
+                {hbarray.map(beast => 
+                    <HornedBeast
+                        title={beast.title}
+                        image_url={beast.image_url}
+                        description={beast.description}
+                        update={this.props.update}
+                        stateColor={this.props.currentColor}
+                    />
+                )}
+             </div>
         )
     }
 };
